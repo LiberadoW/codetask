308ef5f6ddf92c8c863f4eb250037f51fbc3e6ec
Wojciech Szemik <wojciech.szemik@grudnik.pl>
2022-10-10 13:54:31 +0200

    inital upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { GetApiService } from './get-api.service';
+
+type AssignedTo = {
+  person_name?: string;
+  status?: string;
+};
+
+type Data = {
+  work_order_id: number;
+  description: string;
+  received_date: string;
+  assigned_to: AssignedTo[];
+  status: string;
+  priority: string;
+};
+
+const filterData = (array: Data[], filter: string) =>
+  array.filter((el) =>
+    el.description.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+  );
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+  filter: string = '';
+  data: Data[] = [];
+  filteredData: Data[] = [];
+  tableHeaders: string[] = [
+    'WO ID',
+    'Description',
+    'Received date',
+    'Assigned to',
+    'Status',
+    'Priority',
+  ];
+
+  onInput(input: string) {
+    this.filter = input;
+    this.filteredData = filterData(this.data, this.filter);
+  }
+
+  constructor(private api: GetApiService) {}
+
+  ngOnInit() {
+    this.api.getData().then((data:any) => {
+      this.data = data['response']['data'];
+      this.filteredData = data['response']['data'];
+    });
+  }
 }
